acda7d003f976035ee4fe7f5b05ec535597cf5eb
Olli Siivola <olli.siivola@hotmail.com>
2016-11-01 10:28:49 +0200

    Logout toteutettu

--- login.php
+++ login.php
@@ -1,19 +1,13 @@
-<?php include "menu.php";?>
-<form method="post" action="login.php">
-<table border=0>
-	<tr><td>tunnus</td> <td><input type="text" name="tunnus"></td></tr>
-	<tr><td>salasana</td> <td><input type="password" name="salasana"></td></tr>
-	<tr><td></td><td><input type="submit" name="nappi" value="Kirjaudu"></td></tr>
-</table>
-</form>
-
 <?php
+session_start();
 if(isset($_POST['nappi'])) {
 	$oikeatunnus="jeppe";
 	$oikeasalasana="test";
 	if($_POST['tunnus']==$oikeatunnus) {
 		if($_POST['salasana']==$oikeasalasana) {
 			//oikea tunnus ja salasana
+			$_SESSION['kirjautunut']=TRUE;
+			$_SESSION['user']=$oikeatunnus;
 			header('Location:oma.php');
 		}
 		else {
@@ -25,5 +19,16 @@ if(isset($_POST['nappi'])) {
 	}
 }
 ?>
+
+<?php include "menu.php";?>
+<form method="post" action="login.php">
+<table border=0>
+	<tr><td>tunnus</td> <td><input type="text" name="tunnus"></td></tr>
+	<tr><td>salasana</td> <td><input type="password" name="salasana"></td></tr>
+	<tr><td></td><td><input type="submit" name="nappi" value="Kirjaudu"></td></tr>
+</table>
+</form>
+
+
 
 <?php include "footer.php";?>
--- menu.php
+++ menu.php
@@ -15,6 +15,7 @@
 <li><a href="get.php">Get Lomake</a></li>
 <li><a href="array.php">Array</a></li>
 <li><a href="asiakas.php">Asiakkaat</a></li>
+<li><a href="logout.php">Kirjaudu ulos</a></li>
 </ul>
 
 </ul>
